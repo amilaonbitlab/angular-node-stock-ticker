2d5a7cc11e2191db2e2d2a56418bec477e16800e
Amila Sampath <amilaonbit@gmail.com>
2016-07-31 23:06:54 +0530

    Remove node-module and lib dir

--- frontend/js/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * app js 
- */
-(function(){
-	"use strict";
-	
-    angular.module('stockTickerApp', ['ui.router']);
-
-})();
--- public/js/service/constantService.js
+++ public/js/service/constantService.js
@@ -5,8 +5,8 @@
 	 "use strict";
     angular.module('stockTickerApp').constant('constants', {
         SERVER_URL: 'http://localhost:8080',
-        TIME_INTERVEL : 10,
+        TIME_INTERVEL : 20,
         REPEAT_TIMES : 5,
-        WAIT_TIME : 5,
+        WAIT_TIME : 60,
     })
 })();
